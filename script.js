efb9c51134b0585ac3bc67860445f6e8697a1a45
faiq ahmad <faiqa5325@gmail.com>
2023-07-16 10:29:03 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector("#Clock");
+
+// setInterval(function(){},1000)
+setInterval(function () {
+  let date = new Date();
+  //   console.log(date.toLocaleTimeString());
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
